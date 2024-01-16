8941e00368480e76ec1759123a3bf11109901162
ftvalex01 <ftvalex@hotmail.com>
2024-01-16 11:07:15 +0000

    first commit

---
+++ myapp/routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+router.get('/cool',function(res,res){
+  res.send('It is not the richer who has the most but the one who needs the least');
+})
+
+module.exports = router;
