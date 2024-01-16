3860f33a9801baf17af22ba3fc8660297edcd070
ftvalex01 <ftvalex@hotmail.com>
2024-01-16 19:38:04 +0000

    terminada

--- myapp/app.js
+++ myapp/app.js
@@ -1,41 +1,47 @@
 var createError = require('http-errors');
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var favicon = require('serve-favicon');
 
 var app = express();
 
-// view engine setup
+// Configuracion en este caso de las vistas y el motor de las vistas(pug)
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname + '/public/images/favicon.ico')));
 
+// Rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// Manejador de errores
+
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// middleware para el manejo de errores
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Rendereriza la pagina de error
   res.status(err.status || 500);
   res.render('error');
 });
 
+//export del server
 module.exports = app;
